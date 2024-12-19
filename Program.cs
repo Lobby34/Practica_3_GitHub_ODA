33b0ea00601a10d2e66dc5f6461105bb19e6f2de
Oriol <oriolda2004@gmail.com>
2024-12-19 09:32:07 +0100

    Tot arreglat, disculpeu les molèsties

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Model X", "Tesla2", 2023, 300.0);
+        Vehicle vehicle1 = new Cotxe("Model X", "Tesla2", 2023, 300.0, 0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("MustangY", "Tesla2", 1967, 299.0);
+        Vehicle vehicle2 = new Cotxe("MustangY", "Tesla2", 1967, 299.0, 0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
