1ffd4c9f853c3b388924f154795f3d1614d32615
TinFunny <tinfuny@gmail.com>
2025-02-14 13:28:52 +0100

    Update Camio.cs

--- Models/Camio.cs
+++ Models/Camio.cs
@@ -7,7 +7,7 @@ namespace Models
         //CAMIO MODIFICACIOOOOOO
 
 
-        //CAMIO MODIFICAT 2
+        //PROBAAAAAAAAAAAAAA
 
         public Camio(string model, string marca, int any, double velocitatMaxima, int cargaMaxima)
             : base(model, marca, any, velocitatMaxima)
