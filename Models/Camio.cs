58630fedb0f72a76576243843c4e263686201d3a
TinFunny <tinfuny@gmail.com>
2025-02-14 13:13:24 +0100

    2 commit 1r usuari extern

--- Models/Camio.cs
+++ Models/Camio.cs
@@ -7,7 +7,7 @@ namespace Models
         //CAMIO MODIFICACIOOOOOO
 
 
-        //CAMIO MODIFICACIOOOOOOOOOOO 2
+        //CAMIO MODIFICAT 2
 
         public Camio(string model, string marca, int any, double velocitatMaxima, int cargaMaxima)
             : base(model, marca, any, velocitatMaxima)
